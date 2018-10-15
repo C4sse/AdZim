9691d2e6c66672d14f686e533ad49f77ddaebd1b
George Nhari <casse@mac-george.local>
2018-10-15 01:33:32 -0700

    Initial Commit

---
+++ AdZim/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdZim
+//
+//  Created by George Nhari on 15/10/18.
+//  Copyright © 2018 George Nhari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
